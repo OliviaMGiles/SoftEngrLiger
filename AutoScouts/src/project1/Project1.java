d4635da555f824f2754a060997e3d1ddf7a1caf5
Jamsterman25 <Jamsterman25@10.161.1.33>
2017-11-08 10:51:05 -0600

    Testing Commit

---
+++ AutoScouts/src/project1/Project1.java
@@ -0,0 +1,21 @@
+/*
+ *Team Liger 
+*/
+package project1;
+
+/**
+ * Control class
+ * 
+ * @author Olivia
+ */
+public class Project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //UPLOAD TEST - Jeremiah
+    }
+    
+}
