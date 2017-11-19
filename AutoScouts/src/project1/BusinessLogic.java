f2803c8bd2d1771143021311f0349bf84d3f1bf5
Jamsterman25 <Jamsterman25@10.161.1.33>
2017-11-19 11:16:13 -0600

    Added Folds and more action listeners to BusinessLogic

--- AutoScouts/src/project1/BusinessLogic.java
+++ AutoScouts/src/project1/BusinessLogic.java
@@ -6,8 +6,10 @@
 package project1;
 
 import java.awt.CardLayout;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  *
@@ -24,8 +26,20 @@ public class BusinessLogic {
         RestockerGUI restock = new RestockerGUI();
         ManagerGUI manage = new ManagerGUI();
         ScannerInterface scan = new ScannerInterface();
-            
         
+        CardLayout CustomerCards = (CardLayout)checkout.CustomerPanel.getLayout();
+        
+        // SCANNER SETUP - Sets size and mandates top right corner
+        scan.setSize(400,400);
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice defaultScreen = ge.getDefaultScreenDevice();
+        Rectangle rect = defaultScreen.getDefaultConfiguration().getBounds();
+        int x = (int) rect.getMaxX() - scan.getWidth();
+        int y = 0;
+        scan.setLocation(x, y);
+        
+        // MENU ACTION LISTENERS
+        // <editor-fold>
         menu.CheckoutButton.addActionListener((ActionEvent goToCheckout) -> {
             checkout.setVisible(true);
         });
@@ -37,26 +51,78 @@ public class BusinessLogic {
         menu.ManageButton.addActionListener((ActionEvent goToManage) -> {
             manage.setVisible(true);
         });
+        // </editor-fold>
         
-        
-        checkout.ScanItemsButton.addActionListener((ActionEvent OpenItems) -> {
+        // CHECKOUT ACTION LISTENERS
+        // <editor-fold>
+        checkout.ScanItemsButton.addActionListener((ActionEvent openItems) -> {
             scan.setVisible(true);
         });
         
         checkout.CompleteCheckoutButton.addActionListener((ActionEvent goToPayment) -> {
-            CardLayout CustomerCards = (CardLayout)checkout.CustomerPanel.getLayout();
             CustomerCards.show(checkout.CustomerPanel, "PaymentCard");
         });
         
-        checkout.CancelButton.addActionListener((ActionEvent OpenItems) -> {
+        checkout.CancelButton.addActionListener((ActionEvent returnToMenu) -> {
             checkout.setVisible(false);
             menu.setVisible(true);
         });
-  
+        // </editor-fold>
+        
+        // CHECKOUT.PAYMENTCARD ACTION LISTENERS
+        // <editor-fold>
+        checkout.CashButton.addActionListener((ActionEvent goToCash) -> {
+            CustomerCards.show(checkout.CustomerPanel, "CashPanel");
+        });
+        
+        checkout.CardButton.addActionListener((ActionEvent goToCard) -> {
+            CustomerCards.show(checkout.CustomerPanel, "CardPanel");
+        });
+        
         checkout.CancelPaymentButton.addActionListener((ActionEvent goToPayment) -> {
-            CardLayout CustomerCards = (CardLayout)checkout.CustomerPanel.getLayout();
             CustomerCards.show(checkout.CustomerPanel, "CheckoutCard");
         });
+        // </editor-fold>
+        
+        // CHECKOUT.CASHPANEL ACTION LISTENERS
+        // <editor-fold>
+        checkout.CardButton2.addActionListener((ActionEvent goToCard) -> {
+            CustomerCards.show(checkout.CustomerPanel, "CashPanel");
+        });
+        
+        checkout.CancelCash.addActionListener((ActionEvent goToCard) -> {
+            CustomerCards.show(checkout.CustomerPanel, "CardPanel");
+        });
+        // </editor-fold>
+        
+        // CHECKOUT.CASHPANEL MONEY BUTTON ACTION LISTENERS
+        // <editor-fold>
+        
+        // </editor-fold>
+        
+        // CHECKOUT.CARDPANEL ACTION LISTENERS
+        // <editor-fold>
+        checkout.SwipeButton.addActionListener((ActionEvent swipeCard) -> {
+            CustomerCards.show(checkout.CustomerPanel, "CashPanel");
+        });
+        
+        checkout.CashButton2.addActionListener((ActionEvent goToCash) -> {
+            CustomerCards.show(checkout.CustomerPanel, "CashPanel");
+        });
+        
+        checkout.CancelCardButton.addActionListener((ActionEvent returnToCheckout) -> {
+            CustomerCards.show(checkout.CustomerPanel, "CheckoutCard");
+        });
+        // </editor-fold>
+        
+        // RESTOCK ACTION LISTENERS
+        // <editor-fold>
+        
+        // </editor-fold>
      
+        // MANAGE ACTION LISTENERS
+        // <editor-fold>
+        
+        // </editor-fold>
     }
 }
